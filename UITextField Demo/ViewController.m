ed7c6908ad2853e618aa0ab17020692e2172073f
Henry T <tsunglin.tsai@gmail.com>
2013-03-23 12:49:31 -0700

    Complete Implementation

--- UITextField Demo/ViewController.m
+++ UITextField Demo/ViewController.m
@@ -8,22 +8,64 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void) setTextField:(UITextField *)textField{
+    _textField = textField;
+    _textField.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)makeResponserBtnClicked:(id)sender {
+    [self.textField becomeFirstResponder];
 }
+
+- (IBAction)exitResponderBtnClicked:(id)sender {
+    [self.textField resignFirstResponder];
+}
+
+#pragma mark - textview delegate
+
+// Managing Editing
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    NSLog(@"textFieldShouldBeginEditing");
+    return YES;
+}
+
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    NSLog(@"textFieldDidBeginEditing");
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+    NSLog(@"textFieldShouldEndEditing");
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    NSLog(@"textFieldDidEndEditing");
+}
+
+// Editing the Text Field’s Text
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    NSLog(@"shouldChangeCharactersInRange");
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"textFieldShouldReturn");
+    [self.textField resignFirstResponder];
+    return YES;
+}
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField{
+    NSLog(@"textFieldShouldClear");
+    return YES;
+}
+
 
 @end
